21414d437f15db2197d894d16d7680292b899458
xxxjhairxxx <jinfanzonquispe30@gmail.com>
2023-02-01 03:59:40 -0500

    corregir Routes

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,6 +1,6 @@
 import {Routes, Route} from "react-router-dom"
 import Home from "../Pages/Home"
-import UsersResults from "../pages/UsersResults"
+import Usersresults from "../pages/UsersResults"
 import Repositories from "../pages/Repositories"
 import Userresult from "../Pages/UserResult"
 
@@ -12,7 +12,7 @@ export const Router = () => {
     return (  
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/users/:name" element={<UsersResults />} /> 
+                    <Route path="/users/:name" element={<Usersresults />} /> 
                     <Route path="/user/:userName" element={<Userresult />}/>
                     <Route path="/repos/:userName/:repositoriesInfo" element={<Repositories />}/>
                 </Routes>
