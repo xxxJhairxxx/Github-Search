06e71eb501579b0e04d410227c72984aa21fb6d7
xxxjhairxxx <jinfanzonquispe30@gmail.com>
2023-02-01 15:15:18 -0500

    Cambios en las carpetas

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from "react-router-dom"
-import Home from "../pages/home"
+import Home from "../pages/Home"
 import Usersresults from "../pages/usersresults"
 import Repositories from "../pages/repositories"
 import Userresult from "../pages/userresult"
